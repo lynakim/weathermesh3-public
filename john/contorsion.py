9d5566305eecf362a12ccf34bdf4b3e107acbc04
Lyna Kim <56519772+lynakim@users.noreply.github.com>
2025-02-03 17:00:41 -0800

    Delete john directory

--- john/contorsion.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-path = '/fast/proc/era5/f000/199709/873072000.npz'
-data = np.load(path)
-print(data['pr'].shape)
-pass
--- john/mask.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-
-mask = torch.ones(2, 100, 100).tril()
-print(mask.shape)
-print(mask)
